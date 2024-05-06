6d1e75be2f2e3aaacb4ce1fc983a939623e189af
NgJoeYi <s10262850@connect.np.edu.sg>
2024-05-07 05:34:13 +0800

    (Q5) Completed

--- app/src/main/java/sg/edu/np/mad/madpractical4/User.java
+++ app/src/main/java/sg/edu/np/mad/madpractical4/User.java
@@ -1,54 +1,25 @@
 package sg.edu.np.mad.madpractical4;
 
-import java.util.Random;
-
 public class User {
-    private String name;
-    private String description;
-    private int id;
-    private boolean followed;
+    public String name;
+    public String description;
+    public int id;
+    public boolean followed;
 
-    // Constructor with id parameter
     public User(String name, String description, int id, boolean followed) {
         this.name = name;
         this.description = description;
         this.id = id;
         this.followed = followed;
     }
-
-    // Getter and setter for name
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // Getter and setter for description
     public String getDescription() {
         return description;
     }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    // Getter and setter for id
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    // Getter and setter for followed
-    public boolean isFollowed() {
+    public boolean getFollowed() {
         return followed;
-    }
-
-    public void setFollowed(boolean followed) {
-        this.followed = followed;
     }
 }
