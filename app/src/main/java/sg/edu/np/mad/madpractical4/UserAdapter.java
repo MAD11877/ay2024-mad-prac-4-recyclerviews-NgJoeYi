46886f322a6542d0abc2a923041b1eb435de3825
NgJoeYi <s10262850@connect.np.edu.sg>
2024-05-07 06:12:35 +0800

    (Q6) Completed

--- app/src/main/java/sg/edu/np/mad/madpractical4/UserAdapter.java
+++ app/src/main/java/sg/edu/np/mad/madpractical4/UserAdapter.java
@@ -1,4 +1,37 @@
 package sg.edu.np.mad.madpractical4;
 
-public class UserAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
+    ArrayList<User> data;
+
+    public UserAdapter(ArrayList<User> input) {
+        data = input;
+    }
+
+    @Override
+    public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(
+                android.R.layout.simple_list_item_1,
+                parent,
+                false);
+        return new UserViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(UserViewHolder holder, int position) {
+        User user = data.get(position);
+        holder.txt.setText(user.getName()); // Assuming there's a getName() method in your User class
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
 }
