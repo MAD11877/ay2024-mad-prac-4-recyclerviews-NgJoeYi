a643c977f4d5a1bbbab4009713c46fa4456fe3be
NgJoeYi <s10262850@connect.np.edu.sg>
2024-05-07 22:32:27 +0800

    Completed

--- app/src/main/java/sg/edu/np/mad/madpractical4/ListActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical4/ListActivity.java
@@ -1,15 +1,11 @@
 package sg.edu.np.mad.madpractical4;
 
-import android.content.DialogInterface;
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
+
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.helper.widget.Carousel;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -33,58 +29,25 @@ public class ListActivity extends AppCompatActivity {
             return insets;
         });
 
-        // create 20 random users, descriptions, and value
-        ArrayList<User> userArray = new ArrayList<>();
+        //Creating List of 20  users
+        ArrayList<User> userArrayList = new ArrayList<>();
         Random random = new Random();
-        for (int i = 1; i <= 20; i++)
-        {
-            Boolean randomFollow = random.nextBoolean();
-            User user = new User("User" + String.valueOf(random.nextInt(100000)),"Description" + String.valueOf(random.nextInt(100000)),i,randomFollow);
-            userArray.add(user);
+
+        for(int i = 1; i <= 20; i++){
+            Boolean followingRandom = random.nextBoolean();
+
+            User user = new User("User"+String.valueOf(random.nextInt(100000)),"Description"+String.valueOf(random.nextInt(100000)),i,followingRandom);
+            userArrayList.add(user);
         }
 
-        // (Q3) Recycler View
+
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        UserAdapter userAdapter = new UserAdapter(userArray, this);
+        UserAdapter userAdapter =  new UserAdapter(userArrayList, this);
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(userAdapter);
 
 
-        //ImageView profileImg = (ImageView) findViewById(R.id.imageView4);
-
-        //profileImg.setOnClickListener(new View.OnClickListener(){
-            /*public void onClick(View V){
-                AlertDialog.Builder builder = new AlertDialog.Builder(ListActivity.this);
-
-                builder.setTitle("Profile");
-                builder.setMessage("MADness");
-                builder.setCancelable(true);
-                builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Random random = new Random();
-                        int randomNumber = random.nextInt(900000) + 100000;
-
-                        Intent activityName = new Intent(ListActivity.this, MainActivity.class);
-
-                        activityName.putExtra("randomNumber", randomNumber);
-                        startActivity(activityName);
-                        dialog.dismiss();
-                    }
-                });
-                builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-
-                AlertDialog dialog = builder.create();
-
-                dialog.show();
-            }
-        });*/
     }
 }
--- app/src/main/java/sg/edu/np/mad/madpractical4/UserViewHolder.java
+++ app/src/main/java/sg/edu/np/mad/madpractical4/UserViewHolder.java
@@ -1,21 +1,25 @@
 package sg.edu.np.mad.madpractical4;
 
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.recyclerview.widget.RecyclerView;
-
 public class UserViewHolder extends RecyclerView.ViewHolder {
-    ImageView imageViewSmall, imageViewLarge;
-    TextView name, description, txt; // Define the txt variable
 
-    public UserViewHolder(View itemView) {
+    ImageView smallImage, bigImage;
+
+    TextView name;
+
+    TextView description;
+
+    public UserViewHolder(View itemView){
         super(itemView);
+        //location of the xml (the ids)
+        bigImage = itemView.findViewById(R.id.bigImage);
+        smallImage = itemView.findViewById(R.id.profilePic);
         name = itemView.findViewById(R.id.name);
         description = itemView.findViewById(R.id.description);
-        imageViewSmall = itemView.findViewById(R.id.smallRobot);
-        imageViewLarge = itemView.findViewById(R.id.smallBg);
-        txt = itemView.findViewById(android.R.id.text1);
     }
+
 }
